8ea91fc7083dde035084235c42a1dfe2310c271a
Henry Schmale <hschmale16@gmail.com>
2015-01-10 23:33:38 -0500

    Added a default config file for initial configuration. Started work on the major functions of the program

---
+++ Defaults.hpp
@@ -0,0 +1,10 @@
+// Contains the default configuration of program
+// @author Henry Schmale
+// @date January 10, 2015
+
+#ifndef DEFAULTS_H_INCLUDED
+#define DEFAULTS_H_INCLUDED
+
+
+
+#endif // DEFAULTS_H_INCLUDED

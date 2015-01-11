9c06ee4f67f2a527a1ce69bc278484db49248b75
Henry Schmale <hschmale16@gmail.com>
2015-01-11 13:03:01 -0500

    Set up the basics of the database

---
+++ SQL_CMDS.hpp
@@ -0,0 +1,11 @@
+/** contains the necessary sql statements to run the database
+ * @author Henry Schmale
+ * @date January 11, 2015
+ */
+
+#ifndef SQL_CMDS_HPP_INCLUDED
+#define SQL_CMDS_HPP_INCLUDED
+
+
+
+#endif // SQL_CMDS_HPP_INCLUDED

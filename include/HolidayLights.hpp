7b2f85c215ccb468861ec5d2ff224cfec99b6f4a
Henry Schmale <hschmale16@gmail.com>
2015-01-10 23:21:46 -0500

    Added files containing major functions to run the server

---
+++ include/HolidayLights.hpp
@@ -0,0 +1,11 @@
+#ifndef HOLIDAYLIGHTS_HPP_INCLUDED
+#define HOLIDAYLIGHTS_HPP_INCLUDED
+
+
+// Holiday Lights Primary Namespace
+namespace HL
+{
+    void initLights();
+}
+
+#endif // HOLIDAYLIGHTS_HPP_INCLUDED

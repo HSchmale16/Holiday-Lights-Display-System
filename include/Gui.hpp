b4b91f61b21239a93cd8c66a2caf0f1ee664bed8
Henry Schmale <hschmale16@gmail.com>
2015-01-11 21:23:44 -0500

    NCurses Started work on ncurses based gui Reformated install.sh to make columns nice looking

---
+++ include/Gui.hpp
@@ -0,0 +1,18 @@
+/* Functions to control ncurses based GUI
+ * @author Henry Schmale
+ * @date January 11, 2015
+ */
+
+#ifndef GUI_HPP_INCLUDED
+#define GUI_HPP_INCLUDED
+
+namespace gui
+{
+	// Initializes the gui
+    void init();
+
+	// shutsdown the gui
+    void endGui();
+}
+
+#endif // GUI_HPP_INCLUDED
---
+++ src/Gui.cpp
@@ -0,0 +1,17 @@
+/* Functions to control ncurses based GUI
+ * @author Henry Schmale
+ * @date January 11, 2015
+ */
+
+ #include "../include/Gui.hpp"
+ #include <ncurses.h>
+
+ void gui::init()
+ {
+    initscr();
+ }
+
+void gui::endGui()
+{
+
+}

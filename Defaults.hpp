6f7851f4d24624986390957c0779cf3b4be2294c
Henry Schmale <hschmale16@gmail.com>
2015-01-18 14:33:25 -0500

    Documentation of the defaults. Switched from macros to constants for integers. Macros are still in use for string constants as it is cleaner.

--- Defaults.hpp
+++ Defaults.hpp
@@ -1,27 +1,33 @@
-// Contains the default configuration of program
-// @author Henry Schmale
-// @date January 10, 2015
+/** \brief Contains the default configuration of program
+ * \file Defaults.hpp
+ * \author Henry Schmale
+ * \date January 10, 2015
+ */
 
 #ifndef DEFAULTS_H_INCLUDED
 #define DEFAULTS_H_INCLUDED
 
 // EXIT FAIL CODES
-#define SQL_FAIL 1      // SQL Failure
-#define FILE_FAIL 2		// Failed to do something with a file
-#define ARG_FAIL 255	// Something is wrong with the args passed to prog
+const int SQL_FAIL = 1;     //!< SQL Failure
+const int FILE_FAIL =  2;	//!< Failed to do something with a file
+const int ARG_FAIL  = 255;	//!< Something is wrong with the args passed to prog
 
-// File Pathes
-#define DB_FILE_PATH "lights.db"
-#define RESOURCES_PATH "resources/songs/"
+// File Pathes - left as macros because it's cleaner for string constants
+#define DB_FILE_PATH "lights.db" 			//!< Path to the database file
+#define RESOURCES_PATH "resources/songs/"	//!< Path to resources directory
 
 // Update Rates
-#define UPDATE_T_PERIOD 10	// milliseconds to sleep each event loop, reduces
-// CPU Usage
+const int UPDATE_T_PERIOD = 10;	//!< milliseconds to sleep each event loop
 
-#define INTERMISSION 10		// Seconds to wait between shows
+const int INTERMISSION = 10;	//!< Seconds to wait between shows
 
 // Network Defaults
-#define SOCKET_TIMEOUT 150	// milliseconds before socket timesout
+const int SOCKET_TIMEOUT = 150;	//!< milliseconds before socket timesout
+
+// Song Generation Method
+/** \brief Should shows be dynamically generated?
+ */
+const bool DYNAMIC_SHOW_GEN = true;
 
 // Other stuff yet to be thought of, probably config file defaults
 
